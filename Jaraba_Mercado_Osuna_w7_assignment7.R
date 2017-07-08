76b1f6738db8a32c6aa2170aa73bf517fe0c38c9
rosuna80 <RAOsuna@my.harrisburgu.edu>
2017-07-08 09:33:22 -0400

    Add files via upload

---
+++ Jaraba_Mercado_Osuna_w7_assignment7.R
@@ -0,0 +1,19 @@
+#Assignment 7
+
+#1. Populate the missing values in the Area variable with an appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
+
+# ANDREA
+
+#2. Remove special characters, padding (the white space before and after the text) from Street 1 and Street 2 
+#variables. Make sure the first letters of street names are capitalized and the street denominations are 
+#following the same standard (for example, all streets are indicated as "str.", avenues as "ave.", etc.
+
+# RAFAEL 
+
+#3. If the value in Street 2 duplicates the value in Street 1, remove the value in Street                                                                                                                                                                                                                                              
+
+# RAFAEL
+
+#4. Remove the "Strange HTML column"
+
+# ANDREA
